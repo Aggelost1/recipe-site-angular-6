ec23c6c24da9950401750cfcef44c452d4cfd8e4
aggelost1 <aggelost1@gmail.com>
2018-06-18 23:05:51 +0100

    sign in and sign up features created (authorizzation guard and firebase back-end to come)

--- test-app/src/app/core/signin/signin.component.ts
+++ test-app/src/app/core/signin/signin.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+
+
 
 @Component({
-  selector: 'signin',
-  templateUrl: './signin.component.html',
-  
+    selector: 'signin',
+    templateUrl: 'signin.component.html'
 })
-export class SigninComponent {
-  
+export class SigninComponent implements OnInit {
+    myForm: FormGroup;
+    error = false;
+    errorMessage = '';
+
+    constructor(private fb: FormBuilder) {}
+
+    onSignin() {
+           
+    }
+
+    ngOnInit():any {
+        this.myForm = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+    }
 }
